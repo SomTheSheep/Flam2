c07318d9cf00d37de87d4ba4114e324fa16f314f
Som <SohamSalunke007@outlook.com>
2025-11-14 15:20:56 +0530

    Added JNI bridge for image processing

--- app/src/main/jni/native-lib.cpp
+++ app/src/main/jni/native-lib.cpp
@@ -1,11 +1,20 @@
 #include <jni.h>
 #include <string>
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <android/log.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_somthesheep_flam2_MainActivity_stringFromJNI(
+#define LOG_TAG "native-lib"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_somthesheep_flam2_MainActivity_processImage(
         JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+        jobject /* this */,
+        jobject image) {
+
+    // This is a placeholder.
+    // In a real app, you would get the image planes and process them with OpenCV.
+    // For now, we just log that the function was called.
+    LOGD("processImage called");
 }
